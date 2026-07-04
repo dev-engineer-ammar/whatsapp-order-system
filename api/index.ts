a5beb06db035be8adec2b22baa84dfac86e81c9e
muhammadammarjaved <ammar.javid@narsunstudios.com>
2026-07-04 18:09:54 +0500

    gitignore added

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
+import serverless from "serverless-http";
 import app from "../src/app";
 import { connectDB } from "../src/config/database";
 
-// Cache the database connection across serverless invocations
+// cache DB connection
 let isConnected = false;
 
 const ensureConnection = async () => {
@@ -15,10 +15,14 @@ const ensureConnection = async () => {
   }
 };
 
-// Connect to DB before handling requests
-app.use(async (_req, _res, next) => {
+// middleware
+app.use(async (req, res, next) => {
   await ensureConnection();
   next();
 });
 
-export default app;
+// IMPORTANT: wrap express app
+export const handler = serverless(app);
+
+// default export for Vercel
+export default handler;
